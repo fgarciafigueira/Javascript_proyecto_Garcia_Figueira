9ee6b6fc8bd88d68f70eb7caf8c31bbaedc2fe93
Facu Garcia Figueira <fgarciafigueira@gmail.com>
2024-11-05 16:59:05 -0300

    Cambio de trabajo. ahora va a ser un plan de ahorro. ya hice la logica y el 1er estilo

--- js/main.js
+++ js/main.js
@@ -1,71 +1,129 @@
-const nombre= prompt('Ingrese su nombre')
+//Arrays de armado
+const objetivosAhorro = [
+    { id: 1, nombre: "Viaje", precio: 3500 },
+    { id: 2, nombre: "Nueva Notebook", precio: 1000 },
+    { id: 3, nombre: "Cursos de Programación", precio: 600 },
+    { id: 4, nombre: "Auto", precio: 12000 },
+    { id: 5, nombre: "Muebles nuevos", precio: 2500 },
+    { id: 6, nombre: "Departamento", precio: 35000 }
+]
 
-console.log(nombre)
+const porcentajeAhorro= ["0", "5","10", "15", "20", "25", "30"]
 
-let paso=0
+let cartObjetivos = []
 
-const historia=[]
+//La primer funcion arma las cartas de los objetivos
+function imprimirObjetivosEnHTML(objetivosAhorro){
+    let container = document.getElementById("container_objetivos");
 
-historia.push(nombre)
+    for (const objetivo of objetivosAhorro){
+        const card = document.createElement("div")
 
-while (paso<3){
-    const inicioHistoria= prompt('Estas en tu casa viendo la TV. \N suena el telefono y lo levantas.\n Del otro lado te dicen que es una agencia especial y fuiste asignado para una misión, para ello tenes que estar en plaza constitución a las 3..\n ¿Qué vas a hacer? \n 1: Como no quedo claro el horario, vas a las 3 AM a la plaza. \n 2: Vas a las 3 PM, lo logico para encontrarse en esa plaza con alguien. \n 3: Decidis ignorar el mensaje')
-    paso=1
-    
-    switch (parseInt(inicioHistoria)) {
-        case 1:
-            historia.push('Fuiste a las 3 AM al lugar y te rapto una camioneta')
-            paso=2
-            const rama1Historia= prompt('Te rapta una camioneta, pensando que eras "SU" persona y te llevan para robar un museo: \n 1: Te negas. \n 2: Pensas que esta es la mision y accedes. \n 3: Accedes pero con idea de traicionarlos y ser el heroe.')
-        switch(parseInt(rama1Historia)){
-            case 1:
-                historia.push("no se dejan cabos sueltos. te mataron")
-                paso=3
-            break
+        precioConvertido=objetivo.precio * 1150.98
+        precio_formateado=precioConvertido.toLocaleString("es-Ar", {
+            style:"currency",
+            currency:"ARS",
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2
+        })
 
-            case 2:
-                historia.push(" lograste el robo, saliste herido y ahora te busca toda la policia \n ¡Suerte en escapar!")
-                paso=3
-            break
+        card.innerHTML=`<h3>${objetivo.nombre}</h3>
+                        <p>Precio: ${precio_formateado}</p>
+                        <button class="agregarObjetivo" id="${objetivo.id}">Agregar objetivo </button>`
 
-            case 3:
-                historia.push("Hiciste el acto del bien. Evitaste el robo y terminaste conociendo a la gente de la agencia. \n Pero sabes que de alguna forma una venganza vendra")
-                paso=3
-            break
+        container.appendChild(card)
 
-            default:
-                console.log("Opción inválida. ¡Inténtalo de nuevo!")
+    }
+    agregarObjetivoBoton()
+}
+
+imprimirObjetivosEnHTML(objetivosAhorro)
+
+//esta funcion agrega al array cartObjetivos el objetivo deseado
+function agregarObjetivoBoton (){
+     addButton =document.querySelectorAll(".agregarObjetivo")
+     addButton.forEach (button => {
+        button.onclick=(e) =>{
+            const objetivoId= e.currentTarget.id
+            const selectObjetivo = objetivosAhorro.find( objetivos =>objetivos.id ==objetivoId)
+
+            cartObjetivos.push(selectObjetivo)
         }
-        break
+     })
+}
 
-        case 2:
-             historia.push('Te encontraste con los agentes especiales y te dieron la mision')
-             paso=2
-             const rama2Historia=prompt('Te encontraste con los agentes especiales, su mision es evitar el robo del museo y te dan 2 opciones: \n 1: Te infiltras y lo evitas desde adentro de la organizacion de forma silenciosa \n 2: te arman estilo Rambo y sos vos solo contra la organizacion')
-        switch(parseInt(rama2Historia)){
-            case 1:
-                historia.push('te infiltraste bien y terminaste con toda la organizacion. Resulto que era algo mas grande de lo que pensabas y te estan buscando por traicion. ¡Cuidado!')
-                paso=3
-            break
+//Esta funcion imprime los porcentajes que manejamos para armar la consulta
+function imprimirPorcentajesHTML(porcentajeAhorro){
+    let selector = document.getElementById("select_porcentajes")
 
-            case 2:
-                historia.push('Muchos videojuegos hicieron que elijas la opcion de Rambo armado hasta los dientes. ¡Bien Ahí! \nPero no son videojuegos y terminaste como queso Gruyere.')
-                paso=3
-            break
+    for (const porcentaje of porcentajeAhorro){
+        const deplegable = document.createElement("option")
+        deplegable.innerHTML=`<h5 id=${porcentaje} value=${porcentaje}>${porcentaje}%</h5>`
+        selector.appendChild(deplegable)
+      
+    }
 
-            default:
-                alert('Queres traicionar a la agencia? No se puede eso....')
-        }
-        break
+    selector.addEventListener('change', calcular_objetivos)
+}
+imprimirPorcentajesHTML(porcentajeAhorro)
 
-        case 3:
-            historia.push('Te quedaste viendo Shrek 2 en tu casa y no te enteraste de nada')
-            paso=3
-            console.log(paso)
-        break
+const ingresos = document.getElementById('ingresos')
+const selector = document.getElementById('select_porcentajes')
 
-        default:
-            console.log("Opción inválida. ¡Inténtalo de nuevo!")
+
+//aca se desarrolla todos los calculos. y los guardo en una array
+let resultadoObjetivos = []
+function calcular_objetivos (){
+    if(ingresos!='' && cartObjetivos!=''){
+       const totalPrecio= cartObjetivos.reduce((total, objetivo)=>{
+        return total+ objetivo.precio
+       }, 0)
+       const precioConvertido = totalPrecio * 1150.98
+
+       const ingreso =parseFloat(ingresos.value)
+       const porcentaje= parseFloat(selector.value)
+       const porcentajeDecimal = porcentaje /100
+       const ingresoObjetivo=ingreso*porcentajeDecimal
+       const totalCalculo= Math.ceil(parseFloat(precioConvertido / ingresoObjetivo))
+
+       const resultado={
+            precioConvertido: precioConvertido,
+            ingreso: ingreso,
+            porcentaje: porcentaje,
+            ingresoObjetivo: ingresoObjetivo,
+            totalCalculo: totalCalculo
+       }
+       resultadoObjetivos.push(resultado)
+       console.log(resultadoObjetivos)
+       imprimirtablaHTML(resultadoObjetivos)
+
     }
 }
-console.log(historia.join(', '))
+
+function imprimirtablaHTML(resultadoObjetivos){
+    let tabla = document.getElementById("cuerpo_tabla")
+    tabla.innerHTML=""
+    let cabezaTabla = document.getElementById("cabeza_tabla")
+    cabezaTabla.innerHTML=""
+    const head=document.createElement("tr")
+    head.innerHTML=`<th>Total objetivos</th>
+                        <th>Ingresos totales</th>
+                        <th>Porcentaje dedicado</th>
+                        <th>Ingreso dedicado</th>
+                        <th>Meses de pagos</th>`
+        cabezaTabla.appendChild(head)
+    for (const resultado of resultadoObjetivos){
+        
+        
+        const respuesta = document.createElement("tr")
+        respuesta.innerHTML=` <td>${resultado.precioConvertido.toLocaleString("es-Ar", { style: "currency", currency: "ARS" })}</td>
+                              <td>${resultado.ingreso.toLocaleString("es-Ar", { style: "currency", currency: "ARS" })}</td>
+                                <td>${resultado.porcentaje}</td>
+                                <td>${resultado.ingresoObjetivo.toLocaleString("es-Ar", { style: "currency", currency: "ARS" })}</td>
+                                <td>${resultado.totalCalculo}</td>`
+        
+        tabla.appendChild(respuesta)
+    }
+   
+}
+
